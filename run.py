a8b1a8a0148f1edf637eba106ce331e82dd0f5f9
EeeBeeDee <eoghanbehandoyle@gmail.com>
2022-12-01 17:21:26 +0000

    Init commit

--- run.py
+++ run.py
@@ -1,3 +1,48 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+
+
+
+question_list = [
+    {
+        'question': 'Start your adventure! Do you go left, right, forwards',
+        'answers': ['left', 'right', 'forwards']
+    },
+    {
+        'question': 'you see a goblin do you fight, run, talk',
+        'answers': ['a', 'b', 'c']
+    },
+    {
+        'question': '3',
+        'answers': ['a', 'b', 'c']
+    },
+    {
+        'question': '4',
+        'answers': ['a', 'b', 'c']
+    },
+    {
+        'question': '5',
+        'answers': ['a', 'b', 'c']
+    },
+]
+
+
+def ask_question(list):
+    while True:
+        question = input(f"{question_list[list]['question']} \n")
+
+        if question in question_list[list]['answers']:
+            if question == question_list[list]['answers'][0]:
+                print('you turn left')
+                ask_question(1)
+                
+        else: 
+            print('Invalid option. Try again!')
+
+
+
+
+
+
+
+ask_question(0)
