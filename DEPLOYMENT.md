9d963b9789133f50f76baedc3801d143be02e5cf
EeeBeeDee <eoghanbehandoyle@gmail.com>
2022-12-06 01:42:53 +0000

    Add questions in town section of game

---
+++ DEPLOYMENT.md
@@ -0,0 +1,16 @@
+# Deployment
+
+Code Institute provided a [template](https://github.com/Code-Institute-Org/python-essentials-template) to display the terminal view of this backend application in a modern web browser. This is to improve the accessibiltiy of the project to others.
+
+The live deployed application can be found at [Zork and dragons](https://snakes-and-ladders-sw.herokuapp.com/).
+
+[Return to README.md](README.md)
+
+## Local Deployment
+
+A local copy can be made of this repository by typing this command into your terminal in your desired folder.
+- `git clone https://github.com/EeeBeeDee/zork_and_dragons.git`
+
+Or if using gitpod for development you can use this button.
+
+[![Open in Gitpod](https://gitpod.io/button/open-in-gitpod.svg)](https://gitpod.io/#https://github.com/StevenWeir038/Snakes-and-Ladders)
--- questions.py
+++ questions.py
@@ -1,8 +1,9 @@
 from termcolor import colored
+from lists import class_list, party_list, monster_list,  item_list
 
 question_list = [
     {
-        # 0
+        # 0 - beginning
         'question': f"Start your adventure! Do you go {colored('left', 'red')}, {colored('right', 'red')}, {colored('forward', 'red')}",
         'answers': ['left', 'right', 'forwards'],
         'ans1': ['You turn left', 1, 'no_fight'],
@@ -10,7 +11,7 @@ question_list = [
         'ans3': ['You head forwards', 3, 'no_fight']
     },
     {
-        # 1
+        # 1 - goblin
         'question': f"You see a goblin do you {colored('fight', 'red')}, {colored('run', 'red')}, {colored('talk', 'red')}",
         'answers': ['fight', 'run', 'talk'],
         'ans1': ['You turn left', 1, 'fight'],
@@ -18,7 +19,7 @@ question_list = [
         'ans3': ['You try talk to the goblin', 2, 'no_fight']
     },
     {
-        # 2
+        # 2 - goblin - talk
         'question': f"The Goblin demands you give him a health potion if you wish to pass. Do you {colored('fight', 'red')}, {colored('give', 'red')}, {colored('run', 'red')} fight, give, run",
         'answers': ['fight', 'give', 'run'],
         'ans1': ['You turn left', 1, 'fight'],
@@ -27,7 +28,7 @@ question_list = [
         'lose_item': 'health_potion'
     },
     {
-        # 3
+        # 3 - after goblin - see camp
         'question': f"With the goblin behind you, you come across a small campsite littered with a few caravans to your left. You also notice a riverbed to your right. do you try {colored('talk', 'red')} to the camp, check the {colored('river', 'red')}', or press {colored('forward', 'red')}",
         'answers': ['talk', 'river', 'forwards'],
         'ans1': ['You call out to see if anyone is in the camp', 4, 'no_fight'],
@@ -35,19 +36,45 @@ question_list = [
         'ans3': ['You head forwards along the path', 6, 'no_fight']
     },
     {
-        # 4
+        # 4 - checking camp
         'question': f"Two men approach, they seem friendly enough but ou notice a bit of blood on one mans clothes, you exchange pleasantries and tell them why you have set out on your quest. They seem overjoyed that you plan to take down the dragon and offer to let you rest. Do you {colored('rest', 'red')} here, decide to check the {colored('river', 'red')} behind you or press {colored('forward', 'red')}",
         'answers': ['rest', 'river', 'forwards'],
         'ans1': ['You turn left', 1, 'no_fight'],
-        'ans2': ['You turn Right', 5, 'no_fight'],
+        'ans2': ['You double back and cross the path to head towards the river', 5, 'no_fight'],
         'ans3': ['You head forwards', 3, 'no_fight']
     },
     {
-        # 5
+        # 5 - riverbed
         'question': f"Approaching the riverbed you notice a small rickety boat you could potentially use to get down stream. You could also just use it to cross the river and continue on foot. Do you take the {colored('boat', 'red')} down stream, use it to {colored('cross', 'red')}' the river, or turn back and press {colored('forwards', 'red')} along the path you just left",
         'answers': ['boat', 'cross', 'forwards'],
-        'ans1': ['You turn left', 1, 'no_fight'],
+        'ans1': ['You all hop into the boat one at a time trying to capsize it before you even depart. After a few minutes and some wet feet you depart down the river', 6, 'no_fight'],
         'ans2': ['You turn Right', 2, 'no_fight'],
         'ans3': ['You head forwards', 3, 'no_fight']
     },
+     {
+        # 6 - sailing down the river
+        'question': f"After quite some time and smoothing sailing dulling the parties alertness, the party notices the sound of a waterfall, but just too late. The boat rushes over the edge {colored('--BOOM--','white', 'on_red')}, everyone awakes washed onto a shore. you hear the hustle and bustle of a town just ahead and notice a bridge to the right, Do you head towards the {colored('town', 'red')}', decide to take the {colored('bridge', 'red')}, or head back the way you came following the {colored('river', 'red')}",
+        'answers': ['town', 'bridge', 'river'],
+        'ans1': ['You step into the main plaza of the town of Riverstone', 7, 'no_fight'],
+        'ans2': ['Crossing the bridge you end up back where it all began,you decide to rest awhile before starting again', 0, 'no_fight'],
+        'ans3': ['You head forwards', 3, 'no_fight']
+    },
+     {
+        # 7 - entering town 
+        'question': f"As you walk in you notice an inn and a general shop. You ponder whither you have time to rest or shop or should you press on towards the mountain.Do you enter the {colored('inn', 'red')}', decide to {colored('shop', 'red')} or head towards the castle by the {colored('mountain', 'red')}",
+        'answers': ['inn', 'shop', 'mountain'],
+        'ans1': ['You step into the main plaza of the town of Riverstone', 7, 'no_fight'],
+        'ans2': ['Crossing the bridge you end up back where it all began,you decide to rest awhile before starting again', 0, 'no_fight'],
+        'ans3': ['You head forwards', 3, 'no_fight']
+    },
+     {
+        # 8 - inn
+        'question': f"As you walk in you hear a table talking about a man escaping from the dragon castle not long ago. You are greeted by the innkeeper and offered a room for 20 gold. Do you {colored('rest', 'red')}', join the {colored('table', 'red')} to hear more or return to the {colored('town', 'red')} center.",
+        'answers': ['rest', 'table', 'town'],
+        'ans1': ['You step into the main plaza of the town of Riverstone', 7, 'no_fight'],
+        'ans2': ['Crossing the bridge you end up back where it all began,you decide to rest awhile before starting again', 0, 'no_fight'],
+        'ans3': ['You head forwards', 3, 'no_fight']
+    },
+
+
 ]
