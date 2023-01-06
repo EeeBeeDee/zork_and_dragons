40b887888ee017431f32402fe655466daafe685a
EeeBeeDee <eoghanbehandoyle@gmail.com>
2023-01-06 02:09:05 +0000

    Fix lists.py to be pep8 compliant

--- lists.py
+++ lists.py
@@ -6,27 +6,66 @@ class_list = [
         'health_points': 15,
         'skill_points': 10,
         'attack': 3,
-        'skills': [{'name': 'Shield Bash', 'attack': 2, 'special': True, 'dis': 'deals 2 damage and has 50% chance to inflict stun causing opponent to miss next turn. Costs 2SP', 'cost': 2}, 
-        {'name': 'Mighty Blow', 'attack': 5, 'special': False, 'dis': 'You Swing your sword with both hands dealing severe damage. Costs 4SP', 'cost': 4}],
-        'player_name': ''
+        'player_name': '',
+        'skills': [{
+            'name': 'Shield Bash',
+            'attack': 2,
+            'special': True,
+            'dis': (
+                'deals 2 damage and has 50% chance to inflict stun '
+                'causing opponent to miss next turn. Costs 2SP'),
+            'cost': 2},
+            {
+            'name': 'Mighty Blow',
+            'attack': 5,
+            'special': False,
+            'dis': (
+                'You Swing your sword with both hands dealing '
+                'severe damage. Costs 4SP'),
+            'cost': 4
+        }]
     },
     {
         'name': 'mage',
         'health_points': 10,
         'skill_points': 20,
         'attack': 2,
-        'skills': [{'name': 'Fireball', 'attack': 5, 'special': False, 'dis': 'Deals 4 damage to opponent. Costs: 3SP', 'cost': 4},
-        {'name': 'Heal', 'attack': 0, 'special': False, 'dis': 'Heal yourself by 5HP. Costs: 5SP', 'cost': 5}],
-        'player_name': ''
+        'player_name': '',
+        'skills': [{
+            'name': 'Fireball',
+            'attack': 5,
+            'special': False,
+            'dis': (
+                'Deals 4 damage to opponent. Costs: 3SP'),
+            'cost': 4},
+            {
+            'name': 'Heal',
+            'attack': 0,
+            'special': False,
+            'dis': 'Heal yourself by 5HP. Costs: 5SP',
+            'cost': 5
+        }]
     },
     {
         'name': 'archer',
         'health_points': 10,
         'skill_points': 15,
         'attack': 3,
-        'skills': [{'name': 'Double shot', 'attack': 5, 'special': None, 'dis': 'Deals 5 damage. Costs 3SP', 'cost': 3},
-        {'name': 'cripple', 'attack': 3, 'special': True, 'dis': 'Deal damage and 50% chance to stun opponent. Costs: 4SP', 'cost': 4}],
-        'player_name': ''
+        'player_name': '',
+        'skills': [{
+            'name': 'Double shot',
+            'attack': 5,
+            'special': None,
+            'dis': 'Deals 5 damage. Costs 3SP',
+            'cost': 3},
+            {
+            'name': 'cripple',
+            'attack': 3,
+            'special': True,
+            'dis': ('Deal damage and 50% chance to stun '
+                    'opponent. Costs: 4SP'),
+            'cost': 4
+            }]
     }
 ]
 
@@ -38,17 +77,9 @@ monster_list = [
         'name': 'goblin',
         'health_points': 10,
         'attack': 2,
-        'skills': [{'name': 'Slice', 'attack': 5, }]
+        'skills': [{'name': 'Slice', 'attack': 3, }]
     },
-    {
-        'name': 'archer',
-        'health_points': 10,
-        'skill_points': 15,
-        'attack': 3,
-        'skills': [{'name': 'Double shot', 'attack': 5, 'special': None, 'dis': 'Deals 4 damage. Costs 3SP', 'cost': 3},
-        {'name': 'cripple', 'attack': 2, 'special': 3, 'dis': 'Deal damage and also weaken enemy attack by 1. Costs: 4SP', 'cost': 4}],
-        'dex': 5
-    }
+    
 ]
 
 item_list = {
@@ -56,5 +87,3 @@ item_list = {
     'skill_potion': 3,
     'coin': 50
 }
-
-
